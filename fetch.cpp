15801cba3e671decd35f1d1d5a79bbd7c9c0f9d7
İlyas <ashnwor@gmail.com>
2020-06-27 21:50:33 +0300

    misc: code align

--- fetch.cpp
+++ fetch.cpp
@@ -29,12 +29,12 @@ int main() {
   string uptime   = execute("uptime -p");
   string memory   = execute("free -m  | grep ^Mem | tr -s ' ' | cut -d ' ' -f 3") + "M/" + execute("free -m  | grep ^Mem | tr -s ' ' | cut -d ' ' -f 2") + "M";
 
-  cout << username << printRed("@")                          << hostname << endl;
-  cout << setw(16) << left << printRed("dist", true)         << dist     << endl;
-  cout << setw(16) << left << printRed("kernel", true)       << kernel   << endl;
-  cout << setw(16) << left << printRed("cpu", true)          << cpu      << endl;
-  cout << setw(16) << left << printRed("arch", true)         << arch     << endl;
-  cout << setw(16) << left << printRed("uptime", true)       << uptime   << endl;
-  cout << setw(16) << left << printRed("memory", true)       << memory   << endl;
+  cout << username << printRed("@")                    << hostname << endl;
+  cout << setw(16) << left << printRed("dist", true)   << dist     << endl;
+  cout << setw(16) << left << printRed("kernel", true) << kernel   << endl;
+  cout << setw(16) << left << printRed("cpu", true)    << cpu      << endl;
+  cout << setw(16) << left << printRed("arch", true)   << arch     << endl;
+  cout << setw(16) << left << printRed("uptime", true) << uptime   << endl;
+  cout << setw(16) << left << printRed("memory", true) << memory   << endl;
   return 0;
 }
