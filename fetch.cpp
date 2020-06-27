e98a00d04b7acb1e35aa3f291e2740aba95850e0
İlyas <ashnwor@gmail.com>
2020-06-27 18:21:35 +0300

    misc: align some code

--- fetch.cpp
+++ fetch.cpp
@@ -21,12 +21,12 @@ string printRed(string text, bool space = false) {
 int main() {
   string username = execute("whoami");
   string hostname = execute("hostname");
-  string dist = execute("( lsb_release -ds || cat /etc/*release || uname -om ) 2>/dev/null | head -n1 | tr -d '\"'");
-  string kernel = execute("uname -r");
-  string cpu = execute("lscpu | sed -nr '/Model name/ s/.*:\\s*(.*) @ .*/\\1/p'");
-  string arch = execute("uname -m");
-  string uptime = execute("uptime -p");
-  string memory = execute("free -m  | grep ^Mem | tr -s ' ' | cut -d ' ' -f 3") + "M/" + execute("free -m  | grep ^Mem | tr -s ' ' | cut -d ' ' -f 2") + "M";
+  string dist     = execute("( lsb_release -ds || cat /etc/*release || uname -om ) 2>/dev/null | head -n1 | tr -d '\"'");
+  string kernel   = execute("uname -r");
+  string cpu      = execute("lscpu | sed -nr '/Model name/ s/.*:\\s*(.*) @ .*/\\1/p'");
+  string arch     = execute("uname -m");
+  string uptime   = execute("uptime -p");
+  string memory   = execute("free -m  | grep ^Mem | tr -s ' ' | cut -d ' ' -f 3") + "M/" + execute("free -m  | grep ^Mem | tr -s ' ' | cut -d ' ' -f 2") + "M";
 
   cout << username << printRed("@") << hostname << endl;
   cout << printRed("dist", true) << dist << endl;
