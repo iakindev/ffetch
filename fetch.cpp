6dbadbb86e1560b6261571095739599552e4534f
İlyas <ashnwor@gmail.com>
2020-06-27 21:49:25 +0300

    misc: remove CPU substring from cpu

--- fetch.cpp
+++ fetch.cpp
@@ -24,7 +24,7 @@ int main() {
   string hostname = execute("hostname");
   string dist     = execute("( lsb_release -ds || cat /etc/*release || uname -om ) 2>/dev/null | head -n1 | tr -d '\"'");
   string kernel   = execute("uname -r");
-  string cpu      = execute("lscpu | sed -nr '/Model name/ s/.*:\\s*(.*) @ .*/\\1/p'");
+  string cpu      = execute("lscpu | sed -nr '/Model name/ s/.*:\\s*(.*) @ .*/\\1/p' | sed 's#\\CPU##g'");
   string arch     = execute("uname -m");
   string uptime   = execute("uptime -p");
   string memory   = execute("free -m  | grep ^Mem | tr -s ' ' | cut -d ' ' -f 3") + "M/" + execute("free -m  | grep ^Mem | tr -s ' ' | cut -d ' ' -f 2") + "M";
