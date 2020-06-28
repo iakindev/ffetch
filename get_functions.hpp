2018abbd284485da34a430cdc23fa193fb5a6ea3
İlyas <ashnwor@gmail.com>
2020-06-28 19:11:00 +0300

    misc: android fallback

--- get_functions.hpp
+++ get_functions.hpp
@@ -1,10 +1,17 @@
 #pragma once
 #include "utils.hpp"
+#include <iostream>
 #include <regex>
 using namespace std;
 
 string get_cpu() {
   string data = search("/proc/cpuinfo", "model name");
   regex regexp("[mM]odel\\sname\\s+:\\s+");
+  // Android fallback
+  if (data == "") {
+    cout << "hai";
+    data = search("/proc/cpuinfo", "Hardware");
+    regex regexp("Hardware\\s+:\\s+");
+  }
   return regex_replace(data, regexp, "");
 }
